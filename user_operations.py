4aafe89694b481acb0f295ca1f2bd25e94b6a57b
KatanaPlayPr <balahanov.kot@gmail.com>
2026-03-17 22:53:58 +0100

    Enhance user operations with exit options in input prompts; improve clarity and user experience in login, create, delete, and change operations.

--- user_operations.py
+++ user_operations.py
@@ -1,136 +1,204 @@
 import json as j
+
 def give_user_list():
     userNames = get_users_data()[0]
     print("\nListe der Benutzer:")
+
     for userName in userNames:
         print(f"    {userName}")
     print("")
+
 def get_users_data():
     with open("Nutzerdaten.json", "r") as f:
         data = j.load(f)
         userNames = data["userNames"]
         passwords = data["passwords"]
     return userNames, passwords
+
 def rewrite_users_data(userNames, passwords):
     with open("Nutzerdaten.json", "w") as f:
         j.dump({"userNames": userNames, "passwords": passwords}, f)
+
 def login():
     condition = True
+
     while condition:
         currentUserName = input("Geben Sie den Benutzername ein (exit zum Beenden): ")
+
         if currentUserName == "exit":
             condition = False
+
         else:
             password = input("Geben Sie das Passwort ein: ")
+
             if currentUserName in get_users_data()[0] and password in get_users_data()[1]:
                 index = get_users_data()[0].index(currentUserName)
+
                 if password == get_users_data()[1][index]:
                     print("Login erfolgreich!\n")
                 condition = False
+
             else:
                 print("Login fehlgeschlagen. Bitte überprüfen Sie Ihren Benutzernamen und Ihr Passwort.\n")
     return currentUserName
+
 def create_user(currentUserName):
     if currentUserName == "Admin":
         userNames, passwords = get_users_data()
         condition = True    
+
         while condition:
-            newUserName = input("\nGeben Sie den Benutzernamen für den neuen Nutzer ein: ")
-            if userNames.count(newUserName) == 0:
+            newUserName = input("\nGeben Sie den Benutzernamen für den neuen Nutzer ein (exit zum Beenden): ")
+
+            if newUserName == "exit":
+                condition = False
+
+            elif userNames.count(newUserName) == 0:
                 userNames.append(newUserName)
                 passwords.append(input("Geben Sie das Passwort für den neuen Nutzer ein: "))
+
                 rewrite_users_data(userNames, passwords)
                 print("Nutzer erfolgreich erstellt!\n")
+
                 condition = False
+
             else:
                 print("Benutzer mit solche Benutzername schon existiert. Überprüffen Sie die Liste der Nutzer und versuchen Sie es erneut.\n")
+
     else:
         print("\nSie haben keine Berechtigung, Nutzer zu erstellen.\n")
+
 def delete_user(currentUserName):
     if currentUserName == "Admin":
         give_user_list()
+
         userNames, passwords = get_users_data()
         condition = True    
+
         while condition:
-            userNameToDelete = input("Geben Sie den Benutzernamen ein, den Sie löschen möchten: ")
-            if userNameToDelete in userNames and userNameToDelete != "Admin":
+            userNameToDelete = input("Geben Sie den Benutzernamen ein, den Sie löschen möchten (exit zum Beenden): ")
+
+            if userNameToDelete == "exit":
+                condition = False
+
+            elif userNameToDelete in userNames and userNameToDelete != "Admin":
                 index = userNames.index(userNameToDelete)
                 userNames.pop(index)
                 passwords.pop(index)
+
                 rewrite_users_data(userNames, passwords)
                 print(f"Nutzer {userNameToDelete} erfolgreich gelöscht!\n")
+
                 condition = False
+
             elif userNameToDelete == "Admin":
                 print("Der Admin-Benutzer kann nicht gelöscht werden.\n")
+
             else:
                 print("Benutzername nicht gefunden.\n")
+
     else:
         print("\nSie haben keine Berechtigung, Nutzer zu löschen.\n")
+
 def change_password(currentUserName):
     userNames, passwords = get_users_data()
     condition = True    
+
     while condition:
-        oldPassword = input("\nGeben Sie das alte Passwort ein: ")
-        newPassword = input("Geben Sie das neue Passwort ein: ")
-        newPassword1 = input("Geben Sie das neue Passwort nochmal ein: ")
-        if oldPassword == passwords[userNames.index(currentUserName)] and newPassword == newPassword1:
-            passwords[userNames.index(currentUserName)] = newPassword
-            rewrite_users_data(userNames, passwords)
-            print("\nPasswort erfolgreich geändert!\n")
+        oldPassword = input("\nGeben Sie das alte Passwort ein (exit zum Beenden): ")
+
+        if oldPassword == "exit":
             condition = False
+
         else:
-            print("\nEtwas hat nicht geklappt. Überprüffen Sie Ihre Eingaben und versuchen Sie es erneut.\n")
+            newPassword = input("Geben Sie das neue Passwort ein: ")
+            newPassword1 = input("Geben Sie das neue Passwort nochmal ein: ")
+
+            if oldPassword == passwords[userNames.index(currentUserName)] and newPassword == newPassword1:
+                passwords[userNames.index(currentUserName)] = newPassword
+
+                rewrite_users_data(userNames, passwords)
+                print("\nPasswort erfolgreich geändert!\n")
+
+                condition = False
+
+            else:
+                print("\nEtwas hat nicht geklappt. Überprüffen Sie Ihre Eingaben und versuchen Sie es erneut.\n")
+
 def change_user_name(currentUserName):
     userNames, passwords = get_users_data()
+
     if currentUserName == "Admin":
         give_user_list()
         condition = True    
+
         while condition:
-            userNameToChange = input("Geben Sie den Benutzernamen ein, den Sie ändern möchten: ")
-            if userNameToChange in userNames and userNameToChange != "Admin":
+            userNameToChange = input("Geben Sie den Benutzernamen ein, den Sie ändern möchten (exit zum Beenden): ")
+
+            if userNameToChange == "exit":
+                condition = False
+
+            elif userNameToChange in userNames and userNameToChange != "Admin":
                 newUserName = input("Geben Sie den neuen Benutzernamen ein: ")
+
                 if newUserName not in userNames:
                     index = userNames.index(userNameToChange)
                     userNames.pop(index)
                     userNames.insert(index, newUserName)
+
                     rewrite_users_data(userNames, passwords)
                     print(f"Benutzername {userNameToChange} erfolgreich zu {newUserName} geändert!\n")
+
                     condition = False
+
                 else:
                     print("Benutzer mit solche Benutzername schon existiert. Überprüffen Sie die Liste der Nutzer und versuchen Sie es erneut.\n")
+
             elif userNameToChange == "Admin":
                 print("Der Admin-Benutzername kann nicht geändert werden.\n")
+
             else:
                 print("Benutzername nicht gefunden.\n")
+
     else:
         print("\nSie haben keine Berechtigung, Benutzernamen zu ändern.\n")
+
 def user_operations_menu(currentUserName):
     if currentUserName == "exit":
         print("Programm wird beendet. Auf Wiedersehen!")
+
     else:
-        print("Willkommen zum Benutzerverwaltungsmenü!\nHier können Sie folgende Operationen durchführen:\n1: Neuer Benutzer hinterlegen\n2: Benutzer löschen\n3: Ihre Passwort ändern\n4: Benutzernamen ändern\n5: Benutzerliste ausgeben\n6: Logout\n")    #Заменить 5 на возвращение в меню на уровешь выше; возможно заменить на 0; возможно оставить 5, но добавить 0 на этом уровне для выхода из программы без возврата в меню на уровешь выше
         condition = True
+
         while condition:
-            choice = input("Geben Sie die Nummer der gewünschten Operation ein: ")
+            choice = input("Willkommen zum Benutzerverwaltungsmenü!\nHier können Sie folgende Operationen durchführen:\n1: Neuer Benutzer hinterlegen\n2: Benutzer löschen\n3: Ihre Passwort ändern\n4: Benutzernamen ändern\n5: Benutzerliste ausgeben\n6: Logout\nGeben Sie die Nummer der gewünschten Operation ein: ")
+
             try:
                 if choice == "1":
                     print("\nBenutzer erstellen:")
                     create_user(currentUserName)
+
                 elif choice == "2":
                     print("\nBenutzer löschen:")
                     delete_user(currentUserName)
+
                 elif choice == "3":
                     print("\nPasswort ändern:")
                     change_password(currentUserName)
+
                 elif choice == "4":
                     print("\nBenutzernamen ändern:")
                     change_user_name(currentUserName)
+
                 elif choice == "5":
                     give_user_list()
+
                 elif choice == "6":
                     print("Logout erfolgreich!\n")
                     condition = False
+
                 else:
                     print("Ungültige Eingabe. Bitte geben Sie eine Zahl zwischen 1 und 6 ein.\n")
+
             except Exception as e:
                 print(f"Ein Fehler ist aufgetreten: {e}. Bitte versuchen Sie es erneut oder wenden Sie sich an einen Administrator.\n")
