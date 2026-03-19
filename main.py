d8054c1b0270803c9958fe149b266fe966380887
KatanaPlayPr <balahanov.kot@gmail.com>
2026-03-19 11:33:42 +0100

    Added comments and project description to the "main.py"

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-from menu_operations import main_menu as menu
+from menu_operations import main_menu as menu   #main_menu imported
 
-menu()
+
+##################################################
+##                                              ##
+##          Studentendatenbank/                 ##
+##          Student databank                    ##
+##                                              ##
+##################################################
+
+
+# author: 11-19.03.2026 Kostiantyn Balakhanov
+# brief: Student databank with main menu, two
+#        submenus and a number of operations for
+#        operating JSON files with saved data
+# estimated programm size: 444 lines
+
+
+###################################################
+#               Main menu                         #
+# (for more information see "menu_operations.py") #
+###################################################
+
+
+menu()  #main_menu called. This file is actually pretty redundant, I just wanted to have some good looking main file with just one function
