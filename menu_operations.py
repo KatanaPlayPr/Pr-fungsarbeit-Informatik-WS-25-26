a4cf8f7658da27e2a00ecaa5e3029a13952f63e5
KatanaPlayPr <balahanov.kot@gmail.com>
2026-03-19 10:58:08 +0100

    Added comments to "menu_operations.py"

--- menu_operations.py
+++ menu_operations.py
@@ -1,31 +1,31 @@
-from user_operations import login, user_operations_menu as menu_users
-from students_and_grades_operations import students_operations_menu as menu_students
+from user_operations import login, user_operations_menu as menu_users                   #login function imported from the subprogramm (needed for propper functionality of the programm); function for the "user" submenu imported (imported as menu_users for shorter lines and better readability)
+from students_and_grades_operations import students_operations_menu as menu_students    #function for the "students" submenu imported (imported as menu_students for the same reasons)
 
-def main_menu():
-    current_user = login()
+def main_menu():    #function for initialisation of the main menu
+    current_user = login()  #logs user in (for explanation of the function see "user_operations.py") and saves username for checking permissions
 
-    if current_user == str.lower("exit"):
-        print("Programm wird beendet. Auf Wiedersehen!")
+    if current_user == str.lower("exit"):   #case-insensitive cancel-sequence (I just love this wording)
+        print("Programm wird beendet. Auf Wiedersehen!")    #logout message
 
     else:
-        condition = True
+        condition = True    #explained in "user_operations.py"
 
-        while condition:
-            try:
-                choice = input("\nWillkommen zum Hauptmenü!\nHier können Sie in die folgende Untermenüs wechseln:\n1: Benutzerverwaltungsmenü\n2: Studentenverwaltungsmenü\n0: Programm beenden\nGeben Sie die Nummer der gewünschten Operation ein: ")
+        while condition:    #same here
+            try:            #and here
+                choice = input("\nWillkommen zum Hauptmenü!\nHier können Sie in die folgende Untermenüs wechseln:\n1: Benutzerverwaltungsmenü\n2: Studentenverwaltungsmenü\n0: Programm beenden\nGeben Sie die Nummer der gewünschten Operation ein: ")     #asks for the users choice, in which submenu they want to go. Another long line...
 
                 if choice == "1":
-                    menu_users(current_user)
+                    menu_users(current_user)    #function explained in "user_operations.py"
 
                 elif choice == "2":
-                    menu_students(current_user)
+                    menu_students(current_user) #function explained in "students_and_grades_operations.py"
 
-                elif choice == "0":
-                    print("Programm wird beendet. Auf Wiedersehen!")
+                elif choice == "0":     #in this case, opposed to the functions in "user_operations.py" and "students_and_grades_operations.py", "0" input is used for logout, because it is already the main menu
+                    print("Programm wird beendet. Auf Wiedersehen!")    #logout message
                     condition = False
 
                 else:
-                    print("Ungültige Eingabe. Bitte geben Sie eine Zahl zwischen 0 und 2 ein.\n")
+                    print("Ungültige Eingabe. Bitte geben Sie eine Zahl zwischen 0 und 2 ein.\n")   #and of course an error message for those who loves to try not intended inputs :)
 
             except Exception as e:
-                print(f"Ein Fehler ist aufgetreten: {e}. Bitte versuchen Sie es erneut oder wenden Sie sich an einen Administrator.\n")
+                print(f"Ein Fehler ist aufgetreten: {e}. Bitte versuchen Sie es erneut oder wenden Sie sich an einen Administrator.\n")     #for explanation see "user_operations.py"
